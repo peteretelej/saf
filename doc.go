c75ccce641a329ea3fe01d3b36243f04cd00f2aa
Peter Etelej <peter@etelej.com>
2017-03-18 20:29:07 +0300

    Add godoc overview

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package saf provides tools and utilities for Safaricom services.
+The package can be used as a library for custom applications
+or individual saf projects applications can be run.
+*/
+package saf
