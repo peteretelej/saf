c59b83307d62a8cfa586ef5c58f61736a4911103
Peter Etelej <peter@etelej.com>
2017-03-31 22:35:51 +0300

    Add fetch of Bonga balance

--- bundles.go
+++ bundles.go
@@ -2,10 +2,13 @@ package saf
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/fatih/color"
 )
 
 // Public http endpoints for getting required data
@@ -16,10 +19,39 @@ const (
 
 // Bundles defines the structure for bundles values returned
 type Bundles struct {
-	Line    string
-	AccType string
-	Bundles string
-	Airtime float64
+	Line          string
+	AccType       string
+	Bundles       string
+	BundlesExpiry string
+	Airtime       float64
+	BongaSMS      string
+	BongaBalance  string
+}
+
+// PrettyPrint prints bundles information to stdout
+func (b *Bundles) PrettyPrint() {
+	var (
+		head   = color.New(color.FgGreen, color.Bold).Add(color.Underline).SprintFunc()
+		info   = color.New(color.FgWhite, color.BgGreen).SprintFunc()
+		red    = color.New(color.FgRed).SprintFunc()
+		green  = color.New(color.FgGreen).SprintFunc()
+		yellow = color.New(color.FgYellow).SprintFunc()
+	)
+
+	fmt.Printf("\n%s\n", head("SAFARICOM BALANCE"))
+	if b.Line != "" && len(b.Line) < 30 {
+		fmt.Printf("Safcom Line: %s\n", info("0"+b.Line))
+	}
+	airtime := fmt.Sprintf("%.2f", b.Airtime)
+	fmt.Printf("Airtime Balance: %s/=\n", (airtime))
+	fmt.Printf("Data Bundles: %s\n", green(b.Bundles))
+	fmt.Printf("Bundles Expiry: %s\n\n", red(b.BundlesExpiry))
+	if b.BongaSMS != "" {
+		fmt.Printf("Bonga SMS: %s\n", yellow(b.BongaSMS))
+	}
+	if b.BongaBalance != "" {
+		fmt.Printf("Bonga Balance: %s\n", yellow(b.BongaBalance))
+	}
 }
 
 // GetBundles returns the bundles for the line in use
@@ -53,7 +85,14 @@ Especially useful in processing data sent from clients (e.g. xhr)
 func ParseBundles(rawBundles string) (*Bundles, error) {
 	rows := strings.Split(rawBundles, "<tr>")
 
-	res := map[string]string{"Account Types": "", "Data Bundle</span>": "", "Airtime Balances": ""}
+	res := map[string]string{
+		"Account Types":           "",
+		"Data Bundle</span>":      "",
+		"Data Bundle Expiry Date": "",
+		"Airtime Balances":        "",
+		"Bonga SMS</":             "",
+		"Bonga Balance":           "",
+	}
 
 	for _, val := range rows {
 		if !strings.Contains(val, "</tr>") {
@@ -77,19 +116,28 @@ func ParseBundles(rawBundles string) (*Bundles, error) {
 		val2 = strings.Replace(val2, "</td></tr>", "", 1)
 		res[title] = val2
 	}
+	var ok bool
 	for _, val := range res {
-		if val == "" {
-			return nil, errors.New("failed to parse bundles")
+		if val != "" {
+			ok = true // some data found
+			break
 		}
 	}
+	if !ok {
+		return nil, errors.New("Unable to get bundles")
+	}
+
 	airtime, err := strconv.ParseFloat(res["Airtime Balances"], 64)
 	if err != nil {
 		return nil, errors.New("failed to parse bundles: unable to get airtime")
 	}
 	return &Bundles{
-		AccType: res["Account Types"],
-		Bundles: res["Data Bundle</span>"],
-		Airtime: airtime,
+		AccType:       res["Account Types"],
+		Bundles:       res["Data Bundle</span>"],
+		Airtime:       airtime,
+		BundlesExpiry: res["Data Bundle Expiry Date"],
+		BongaSMS:      res["Bonga SMS</"],
+		BongaBalance:  res["Bonga Balance"],
 	}, nil
 
 }
--- cmd/bundles/main.go
+++ cmd/bundles/main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"github.com/fatih/color"
+	"fmt"
+
 	"github.com/peteretelej/saf"
 )
 
 func main() {
 	b, err := saf.GetBundles()
 	if err != nil {
-		color.Red("Failed to get bundles: %v", err)
+		fmt.Printf("Failed to get bundles: %v\n", err)
 		return
 	}
+	b.PrettyPrint()
 
-	c := color.New(color.FgGreen).Add(color.Underline)
-	c.Println("SAFARICOM BALANCE")
-	color.White("Safcom Line: 0%s", b.Line)
-	c = color.New(color.FgGreen).Add(color.Bold)
-	c.Printf("Data Bundles: %s\n", b.Bundles)
-	c = color.New(color.FgYellow).Add(color.Bold)
-	c.Printf("Airtime Bal: %.2f /=\n", b.Airtime)
 }
